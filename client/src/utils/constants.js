23b07967218ec8a2eaec152c2cfb3a49f3d9db1d
Saif ur Rehman <saifr@seed.im>
2023-02-07 19:57:38 +0500

    update

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -2,4 +2,4 @@ import abi from "./Transaction.json"
 
 
 export const contractABI     = abi.abi;
-export const contractAddress = '0xFbd599781488c730c7cdb3EE96816d1B8E202996';
+export const contractAddress = '';
